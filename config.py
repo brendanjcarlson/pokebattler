27b58b1fade649b67fb0f5e2a666fa3f482fe72e
Brendan Carlson <108992158+brendanjcarlson@users.noreply.github.com>
2023-09-21 19:23:58 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__name__))
 
 class Config:
-    FLASK_APP = os.environ.get('FLASK_APP')
-    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    FLASK_APP = os.environ.get('FLASK_APP') or "app"
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG') or 0
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "1234"
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
